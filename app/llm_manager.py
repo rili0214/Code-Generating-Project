a13c5d80d4a892f705fdc4b45247f35f24dafe98
rili0214 <62629102+rili0214@users.noreply.github.com>
2024-11-12 13:19:41 -0500

    Update repo
    Renaming, modified llm_manager

--- app/llm_manager.py
+++ app/llm_manager.py
@@ -1,22 +1,21 @@
 # llm_manager.py
 import logging
 from pathlib import Path
-from generator import azure_openai_generate_code
-from LLMs.llama_31_nemotron_70b.generate import generate_code as llama_generate_code
-from LLMs.deepseek_coder_v2.generate import generate_code as deepseek_generate_code
-from LLMs.qwen2_5_72b_instruct.generate import generate_code as qwen_generate_code
+from LLMs.azure_openai.openai_generate import azure_openai_generate_code
+from LLMs.llama.generate import generate_code as llama_generate_code
+from LLMs.qwen.generate import generate_code as qwen_generate_code
+from LLMs.dafny_generator.dafny_generate import generate_code as dafny_generate_code
 
 # Set up logging
 log_file_path = Path(__file__).parent.parent / 'logs' / 'logs.txt'
 logging.basicConfig(filename=log_file_path, level=logging.INFO, format='%(asctime)s - %(levelname)s - %(message)s')
 logger = logging.getLogger(__name__)
 
-# Define a dictionary to map model names to their generate functions
+# Dictionary to map model names to their generate functions
 llm_generators = {
-    'llama_31_nemotron_70b': llama_generate_code,
-    'deepseek_coder_v2': deepseek_generate_code,
-    'qwen2_5_72b_instruct': qwen_generate_code,
-    'azure_openai_gpt4': azure_openai_generate_code  # New option for Azure's GPT-4 model
+    'llama_3_1_70B_Inst': llama_generate_code,
+    'qwen2_5_72B_instruct': qwen_generate_code,
+    'openai_gpt4o_mini': azure_openai_generate_code
 }
 
 def generate_code_with_llms(code_input, model_name):
